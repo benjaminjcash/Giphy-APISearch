1a296e9501fe861531fc4bc8aec2e453bdcc8e42
binajaminallnite@gmail.com^C <binjaminallnite.com^C>
2018-01-07 23:05:25 -0700

    finalizing javascript

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,7 @@ var topics = ["The Beatles", "The Doors", "Grateful Dead", "Jefferson Airplane"]
 
 function renderButtons() {
     $("#button-box").empty();
-    for (i=0; i<topics.length; i++) {
+    for (let i=0; i<topics.length; i++) {
         var newButton = $("<button>");
         newButton.addClass("searchterms btn btn-default");
         newButton.attr("data-name", topics[i]);
@@ -10,55 +10,59 @@ function renderButtons() {
         $("#button-box").append(newButton);
     }
     console.log(topics)
+
+    $(".searchterms").on("click", function () {
+        console.log("test")
+        $("#gif-box").empty();
+        var searchTerm = $(this).attr("data-name");
+        searchTerm = searchTerm.replace(" ", "+");
+        $.ajax({
+            url: queryURL + "q=" + searchTerm,
+            method: "GET"
+        }).done(function (response) {
+            console.log(response)
+            var imgArr = response.data
+            for (i = 0; i < imgArr.length; i++) {
+                var fixedImgURL = response.data[i].images.fixed_width_still.url
+                var movingImgURL = response.data[i].images.fixed_width.url
+                var rating = response.data[i].rating
+                var newImg = $("<img>");
+                newImg.attr({
+                    "src": fixedImgURL,
+                    "data-still": fixedImgURL,
+                    "data-animate": movingImgURL,
+                    "data-state": "still",
+                    "width": "50%",
+                    "class": "gifs"
+                });
+                var ratingText = $("<p>");
+                ratingText.text("Rating: " + rating);
+                var newDiv = $("<div>");
+                newDiv.attr({
+                    "class": "well",
+                    "width": "40%",
+                });
+                newDiv.append(newImg);
+                newDiv.prepend(ratingText);
+                $("#gif-box").prepend(newDiv);
+            }
+            $(".gifs").on("click", function () {
+                var state = $(this).attr("data-state");
+                if (state === "still") {
+                    $(this).attr("src", $(this).attr("data-animate"));
+                    $(this).attr("data-state", "animate");
+                } else {
+                    $(this).attr("src", $(this).attr("data-still"));
+                    $(this).attr("data-state", "still");
+                }
+            });
+        });
+    })
 }
 
 renderButtons();
 
 var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=hXLKYWsEie9tH97LMJNLhV0fcAJghusc&limit=10&"
-
-$(".searchterms").on("click", function(){
-    console.log("test")
-    $("#gif-box").empty();
-    var searchTerm = $(this).attr("data-name");
-    searchTerm = searchTerm.replace(" ", "+");
-    $.ajax({
-        url: queryURL + "q=" + searchTerm,
-        method: "GET"
-    }).done(function (response) {
-        console.log(response)
-        var imgArr = response.data
-        for (i=0; i<imgArr.length; i++){
-            var fixedImgURL = response.data[i].images.fixed_width_still.url
-            var movingImgURL = response.data[i].images.fixed_width.url
-            var newImg = $("<img>");
-            newImg.attr({
-                "src" : fixedImgURL,
-                "data-still" : fixedImgURL,
-                "data-animate" : movingImgURL,
-                "data-state" : "still",
-                "width" : "50%",
-                "class" : "gifs"
-            });
-            var newDiv = $("<div>");
-            newDiv.attr({
-                "class" : "well",
-                "width" : "40%",
-            })
-            newDiv.append(newImg);
-            $("#gif-box").prepend(newDiv);
-        }
-        $(".gifs").on("click", function () {
-            var state = $(this).attr("data-state");
-            if (state === "still") {
-                $(this).attr("src", $(this).attr("data-animate"));
-                $(this).attr("data-state", "animate");
-            } else {
-                $(this).attr("src", $(this).attr("data-still"));
-                $(this).attr("data-state", "still");
-            }
-        });
-    });
-})
 
 $("#add-search-term").on("click", function (event) {
     event.preventDefault();
